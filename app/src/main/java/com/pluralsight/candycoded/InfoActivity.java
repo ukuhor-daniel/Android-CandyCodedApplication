64907bd4e9da6d27097bf9d23535c341f7d0fa07
ukuhor-daniel <eloh.danielz@gmail.com>
2020-10-01 03:28:31 +0100

    fourth commit

--- app/src/main/java/com/pluralsight/candycoded/InfoActivity.java
+++ app/src/main/java/com/pluralsight/candycoded/InfoActivity.java
@@ -32,6 +32,12 @@ public class InfoActivity extends AppCompatActivity {
             startActivity(mapIntent);
         }
     }
+    public void createPhoneIntent(View view){
+        Intent phoneIntent = new Intent(Intent.ACTION_DIAL);
+        Uri phoneUri = Uri.parse("tel:0123456789");
+        phoneIntent.setData(phoneUri);
+        startActivity(phoneIntent);
+    }
 
     // ***
     // TODO - Task 2 - Launch the Google Maps Activity
